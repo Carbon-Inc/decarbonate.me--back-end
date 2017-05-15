1eca5b76c48156537003d31e788f6749efb31204
Cameron Bacon <cameronmbacon@gmail.com>
2017-05-15 16:15:57 -0700

    scaffolded app and package.json

---
+++ server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('dotenv').load();
+
+const express = require('express');
+const cors = require('cors');
+const debug = require('debug')('decarbonate:server');
+const morgan = require('morgan');
+const Promise = require('bluebird');
+const bodyParser = require('body-parser').json();
+const mongoose = require('mongoose');
+
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/decarbonate-env';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser);
+app.use('/decarbonate', require('./routes/eb-routes')(router));
+
+const server = module.exports = app.listen(PORT, () => debug(`Listening on ${PORT}`));
+
+server.isRunning = true;
