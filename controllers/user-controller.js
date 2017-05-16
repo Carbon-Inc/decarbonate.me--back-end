e32546749c146f528f25f8ec955d0c079846f1a4
Steven Johnson <stevenjohnson86@gmail.com>
2017-05-16 14:54:47 -0700

    add basic create and fetch methods for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,15 +4,23 @@ const debug = require('debug')('decarb: user-controller');
 const User = require('../models/user');
 const createError = require('http-errors');
 const Promise = require('bluebird');
-const mongoose = require('mongoose');
 
 module.exports = exports = {};
 
-exports.fetchUser = function(accCode){
-  if(!accCode) return Promise.reject(createError(400, 'Access Code required.'));
+exports.createUser = function(userData){
+  debug('#createUser');
+  if(!userData) return Promise.reject(createError(400, 'User data required.'));
 
-  //send accCode to evBrite controller(orRoute)?
-  //check returned oaToken against User model,
-  //if user exists, <something>, else createUser?
-  //return oaToken to route
+  return new User(userData)
+  .then(user => Promise.resolve(user))
+  .catch(err => Promise.reject(createError(err.status, err.name)));
+};
+
+exports.fetchUser = function(evBUserId){
+  debug('#fetchUser');
+  if(!evBUserId) return Promise.reject(createError(400, 'EventBrite User Id required'));
+
+  return User.find({userId: evBUserId})
+  .then(user => Promise.resolve(user))
+  .catch(err => Promise.reject(createError(err.status, err.name)));
 };
