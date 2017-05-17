2d14335e48badf2c803885bb6599bd8800990790
Cameron Bacon <cameronmbacon@gmail.com>
2017-05-17 07:05:02 -0700

    refactored EB routes to a single call, added GET for our API

--- controllers/eb-controller.js
+++ controllers/eb-controller.js
@@ -6,53 +6,34 @@ const Event = require('../models/event');
 
 module.exports = exports = {};
 
-exports.getEventIds = function(body) {
-  debug('#getEventIds');
-  body = JSON.parse(body);
-  let orders = body.orders;
-  let eventIds = orders.map(ele => ele.event_id);
-
-  return Promise.resolve(eventIds);
-};
-
-exports.getEventInfo = function(event) {
+exports.getEventInfo = function(body) {
   debug('#getEventInfo');
-
-  event = JSON.parse(event);
-  let eventObj = {
-    name: event.name.text,
-    description: event.description.text,
-    start: event.start.local,
-    end: event.end.local,
-    eventId: event.id,
-    venueId: event.venue_id,
-    logoId: event.logo_id,
-    categoryId: event.category_id,
-  };
-  return Promise.resolve(eventObj);
+  body = JSON.parse(body);
+  let events = [];
+  body.orders.forEach(ele => {
+    let eventObj = {
+      name: ele.event.name.text,
+      description: ele.event.description.text,
+      img: ele.event.logo.original.url,
+      category: ele.event.category.name,
+      start: ele.event.start.local,
+      end: ele.event.end.local,
+      address: ele.event.venue.address.localized_address_display,
+      eventId: ele.event_id,
+      venueId: ele.event.venue_id,
+      logoId: ele.event.logo_id,
+      categoryId: ele.event.category_id,
+    };
+    events.push(eventObj);
+  });
+  return Promise.resolve(events);
 };
 
-exports.getVenueInfo = function(venue, id) {
-  debug('#getVenueInfo');
-  venue = JSON.parse(venue);
-  return Event.findByIdAndUpdate(id, {address: venue.address.localized_address_display}, {new: true});
-};
-
-exports.getLogoInfo = function(media, id) {
-  debug('#getLogoInfo');
-  media = JSON.parse(media);
-  return Event.findByIdAndUpdate(id, {img: media.original.url}, {new: true});
-};
-
-exports.getCategoryInfo = function(category, id) {
-  debug('#getCategoryInfo');
-  category = JSON.parse(category);
-  return Event.findByIdAndUpdate(id, {category: category.name}, {new: true});
-};
-
-exports.createEvent = function(newEvent) {
+exports.createEvent = function(newEvents) {
   debug('#createEvent');
-  return new Event(newEvent).save();
+  newEvents.forEach(ele => {
+    return new Event(ele).save();
+  });
 };
 
 exports.fetchEvents = function() {
--- routes/eb-routes.js
+++ routes/eb-routes.js
@@ -8,58 +8,17 @@ const ebURL = 'https://www.eventbriteapi.com/v3';
 const EB_ACCESS_TOKEN = process.env.EB_ACCESS_TOKEN;
 
 module.exports = function() {
+  debug('#ebRouter');
   return new Promise((resolve, reject) => {
-    superagent.get(`${ebURL}/users/me/orders`)
+    superagent.get(`${ebURL}/users/me/orders/?expand=event.venue,event.category,event.logo`)
     .set('Authorization', `Bearer ${EB_ACCESS_TOKEN}`)
     .then(res => {
-      debug('#GET /users/me/orders');
-      return ebController.getEventIds(res.text);
+      return ebController.getEventInfo(res.text);
     })
-    .then(eventIds => {
-      eventIds.forEach(ele => {
-        debug('#GET /events/:id');
-        superagent.get(`${ebURL}/events/${ele}`)
-        .set('Authorization', `Bearer ${EB_ACCESS_TOKEN}`)
-        .then(res => {
-          return ebController.getEventInfo(res.text);
-        })
-        .then(newEvent => {
-          return ebController.createEvent(newEvent);
-        })
-        .then(eventObj => {
-          debug('#GET /venues/:id');
-          superagent.get(`${ebURL}/venues/${eventObj.venueId}`)
-          .set('Authorization', `Bearer ${EB_ACCESS_TOKEN}`)
-          .then(res => {
-            return ebController.getVenueInfo(res.text, eventObj._id);
-          })
-          .then(() => {
-            debug('#GET /media/:id');
-            superagent.get(`${ebURL}/media/${eventObj.logoId}`)
-            .set('Authorization', `Bearer ${EB_ACCESS_TOKEN}`)
-            .then(res => {
-              return ebController.getLogoInfo(res.text, eventObj._id);
-            })
-            .then(() => {
-              debug('#GET /categories/:id');
-              superagent.get(`${ebURL}/categories/${eventObj.categoryId}`)
-              .set('Authorization', `Bearer ${EB_ACCESS_TOKEN}`)
-              .then(res => {
-                return ebController.getCategoryInfo(res.text, eventObj._id);
-              })
-              .then(() => {
-                return ebController.fetchEvents();
-              })
-              .then(events => resolve(events))
-              .catch(err => reject(err));
-            })
-            .catch(err => reject(err));
-          })
-          .catch(err => reject(err));
-        })
-        .catch(err => reject(err));
-      });
+    .then(events => {
+      return ebController.createEvent(events);
     })
+    .then(() => resolve())
     .catch(err => reject(err));
   });
 };
