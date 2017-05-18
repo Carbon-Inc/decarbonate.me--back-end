0a021f0eb184d7fddd34a3173181c7cedf8466b9
Steven Johnson <stevenjohnson86@gmail.com>
2017-05-17 18:18:00 -0700

    added some tests for footprint calculation routes

--- test/base-routes-test.js
+++ test/base-routes-test.js
@@ -17,7 +17,7 @@ describe('event-base-routes', function() {
       .post('/decarbonate/events')
       .end((err, res) => {
         if(err) done(err);
-        expect(res).to.have.property('status', 200);
+        expect(res).to.have.status(200);
         done();
       });
     });
@@ -25,7 +25,7 @@ describe('event-base-routes', function() {
       chai.request(server)
       .post('/decarbonate/badroute')
       .end((err, res) => {
-        expect(res).to.have.property('status', 404);
+        expect(res).to.have.status(404);
         done();
       });
     });
@@ -38,15 +38,114 @@ describe('event-base-routes', function() {
       .get('/decarbonate/events')
       .end((err, res) => {
         if(err) done(err);
-        expect(res).to.have.property('status', 200);
+        expect(res).to.have.status(200);
         done();
       });
     });
     it('should return 404 if given bad route', done => {
       chai.request(server)
-      .get('decarbonate/badroute')
+      .get('/decarbonate/badroute')
       .end((err, res) => {
-        expect(res).to.have.property('status', 404);
+        expect(res).to.have.status(404);
+        done();
+      });
+    });
+  });
+
+  describe('#POST /footprint/automobile', function(){
+    it('should return 404 on bad route', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/badroute')
+      .set('testDate', '01-01-2018')
+      .set('distance', 100)
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+    });
+    it('should return 200 on proper request', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/automobile')
+      .set('testDate', '01-01-2018')
+      .set('distance', 100)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+    });
+    it('should return 400 on bad request', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/automobile')
+      .set('testDate', null)
+      .set('distance', 100)
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        done();
+      });
+    });
+  });
+
+  describe('#POST /footprint/bus', function(){
+    it('should return 404 on bad route', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/badroute')
+      .set('testDate', '01-01-2018')
+      .set('distance', 100)
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+    });
+    it('should return 200 on proper request', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/bus')
+      .set('testDate', '01-01-2018')
+      .set('distance', 100)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+    });
+    it('should return 400 on bad request', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/bus')
+      .set('testDate', null)
+      .set('distance', 100)
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        done();
+      });
+    });
+  });
+
+  describe('#POST /footprint/plane', function(){
+    it('should return 404 on bad route', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/badroute')
+      .set('testDate', '01-01-2018')
+      .set('distance', 100)
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+    });
+    it('should return 200 on proper request', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/plane')
+      .set('testDate', '01-01-2018')
+      .set('distance', 200)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+    });
+    it('should return 400 on bad request', done => {
+      chai.request(server)
+      .post('/decarbonate/footprint/plane')
+      .set('testDate', null)
+      .set('distance', 100)
+      .end((err, res) => {
+        expect(res).to.have.status(400);
         done();
       });
     });
