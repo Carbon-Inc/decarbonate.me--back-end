f49ffb58cdb7b2fa9645db7d6524311522c8ea66
Cameron Bacon <cameronmbacon@gmail.com>
2017-05-18 08:02:43 -0700

    added oauth routes for server-side flow

--- routes/base-routes.js
+++ routes/base-routes.js
@@ -1,11 +1,35 @@
 'use strict';
 
 const debug = require('debug')('decarbonate:base-routes');
+const superagent = require('superagent-bluebird-promise');
 const ebRouter = require('./eb-routes');
 const ebController = require('../controllers/eb-controller');
 const bpRouter = require('./brighter-planet-routes');
+const CLIENT_KEY = process.env.CLIENT_KEY;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 module.exports = function(router) {
+  router.post('/oauth', (req, res) => {
+    debug('#POST /decarbonate/oauth');
+    superagent.get(`https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=${CLIENT_KEY}`)
+    .then(() => {
+      res.send();
+    })
+    .catch(err => res.send(err));
+  });
+
+  router.get('/token', (req, res) => {
+    debug('#POST /decarbonate/token');
+    console.log(window.location.search.split('=')[1]);
+    superagent.post(`https://www.eventbrite.com/oauth/token?code=${window.location.search.split('=')[1]}&client_secret=${CLIENT_SECRET}&client_id=${CLIENT_KEY}&grant_type=authorization_code`)
+    .set('Content-type', 'application/x-www-form-urlencoded')
+    .then(data => {
+      console.log(data);
+      res.send(data);
+    })
+    .catch(err => res.sendStatus(err.status));
+  });
+
   router.post('/events', (req, res) => {
     debug('#POST /decarbonate/events');
     return ebRouter()
